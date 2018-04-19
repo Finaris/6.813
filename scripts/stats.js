6e3ce34c882bc9ac0c008804b38a3b0f413725a5
Elijah Stiles <elijahstiles1@gmail.com>
2018-04-19 18:04:57 -0400

    Added a lot of stats stuff

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,4 +1,5 @@
-dom = {};
+
+let userLists = getMyListsData();
 
 Util.events(document, {
 	// This runs when the DOM is ready.
@@ -10,6 +11,7 @@ Util.events(document, {
 });
 
 function initStatsPageDOM() {
+  let dom = {};
   dom.totalShows = Util.one('#total-shows');
   dom.completedShows = Util.one('#completed-shows');
   dom.totalEpisodes = Util.one('#total-episodes');
@@ -23,9 +25,56 @@ function initStatsPageDOM() {
   dom.metaAvgOverallRating = Util.one('#meta-avg-rating');
   dom.yourAvgOverallRating = Util.one('#your-avg-rating');
 
-  var stats = getStatsData();
+  let shows = userLists["All Dramas"];
+  let genreCounts = getGenreCountDict(shows);
+  let stats = getStatsData(shows, genreCounts);
+  initBarChart(genreCounts);
 
   for (var key in stats) {
     dom[key].textContent=stats[key];
   }
+}
+
+function getStatsData(shows, genreCounts) {
+  let stats = {
+    totalShows: shows.length,
+    completedShows: shows.reduce((total, show) => total + (show.numEpisodes == show.userCurrentEpisode ? 1 : 0), 0),
+    totalEpisodes: shows.reduce((total, show) => total + show.numEpisodes, 0),
+    completedEpisodes: shows.reduce((total, show) => total + show.userCurrentEpisode, 0),
+    totalHours: 0,
+    completedHours: 0,
+    avgEpisodesPerWeek: 0,
+    avgHoursPerWeek: 0,
+    favoriteGenre: Object.keys(genreCounts).reduce((a, b) => genreCounts[a] > genreCounts[b] ? a : b),
+    favoriteActor: generateElementsFromArray(actors, 1, 1)[0],
+    metaAvgOverallRating: (shows.reduce((total, show) => total + Number(show.rating), 0) / shows.length).toFixed(1),
+    yourAvgOverallRating: (shows.reduce((total, show) => total + Number(show.userRating), 0) / shows.length).toFixed(1)
+  }
+  stats.totalHours = (stats.totalEpisodes * 63 / 60).toFixed(1);
+  stats.completedHours = (stats.completedEpisodes * 63 / 60).toFixed(1);
+  stats.avgEpisodesPerWeek = (stats.completedEpisodes / 52).toFixed(1);
+  stats.avgHoursPerWeek = (stats.completedHours / 52).toFixed(1);
+  return stats;
+}
+
+function getGenreCountDict(shows) {
+  let genreCounts = {};
+  for (let show of shows) {
+    for (let genre of show.genres) {
+      if (!(genre in genreCounts)) {
+        genreCounts[genre] = 0;
+      }
+      genreCounts[genre] += 1;
+    }
+  }
+  return genreCounts;
+}
+
+function initBarChart(genreCounts) {
+  let data = [{
+    x: Object.keys(genreCounts),
+    y: Object.values(genreCounts),
+    type: 'bar',
+  }];
+  Plotly.newPlot('bar-chart', data);
 }
