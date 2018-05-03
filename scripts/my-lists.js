97406f2d6a982b8b1bc7fe9f5f1dd47184a2511f
Elijah Stiles <elijahstiles1@gmail.com>
2018-05-03 13:23:19 -0400

    Working on My Lists page show display

--- scripts/my-lists.js
+++ scripts/my-lists.js
@@ -5,15 +5,16 @@ let idNamesToDisplayNames = getIdNamesToDisplayNames();
 Util.events(document, {
 	// This runs when the DOM is ready.
     "DOMContentLoaded": function() {
-
-      onListBtnClick('towatch-btn');
+      initDOM();
       initListeners();
+      onListBtnClick('towatch-btn');
     },
 });
 
 function onListBtnClick(listBtnId) {
   updateTabBorders(listBtnId);
-  updateMainBar(listBtnId);
+  clearHeaderShowBars();
+  updateBars(listBtnId);
 }
 
 function initListeners() {
@@ -25,6 +26,12 @@ function initListeners() {
       }
     });
   }
+}
+
+function initDOM() {
+  let showSectionElm = Util.one('#shows-section');
+  showSectionElm.removeChild(showSectionElm.childNodes.item(2));
+  showSectionElm.removeChild(showSectionElm.childNodes.item(0));
 }
 
 //------------------------------------------------------- Helper Functions -----------------------------------------------------------//
@@ -40,16 +47,44 @@ function updateTabBorders(listBtnId) {
   }
 }
 
-function updateMainBar(listBtnId) {
-  let listIdName = listBtnId.slice(0,-4);
-  Util.one('#current-list-name').innerHTML = idNamesToDisplayNames[listIdName];
-  Util.one('#num-elms-in-list').innerHTML = userLists[idNamesToDisplayNames[listIdName]].All.length;
-}
-
-function updateHeaderShowBars(listBtnId) {
+function updateBars(listBtnId) {
+  let showSectionElm = Util.one('#shows-section');
   
+  let listIdName = listBtnId.slice(0,-4);
+  let listDisplayName = idNamesToDisplayNames[listIdName];
+  
+  // Update main bar
+  Util.one('#current-list-name').innerHTML = listDisplayName;
+  Util.one('#num-elms-in-list').innerHTML = userLists[listDisplayName].All.length;
+  
+  // Update header and show bars
+  let listHeaderDict = userLists[listDisplayName];
+  for (let header in listHeaderDict) {
+    if (header == 'All') {
+      continue;
+    }
+    showSectionElm.appendChild(getHeaderBarElm(header, listHeaderDict[header].length));
+    let shows = listHeaderDict[header]; // Sorting shows within header would go here
+    for (let show of shows) {
+      showSectionElm.appendChild(getShowBarElm(show));
+    }
+  }
 }
 
 function clearHeaderShowBars() {
-  
+  let showSectionElm = Util.one('#shows-section');
+  let mainBarElm = showSectionElm.firstChild;
+  while (mainBarElm.nextSibling != null) {
+    showSectionElm.removeChild(mainBarElm.nextSibling);
+  }
+}
+
+function getHeaderBarElm(headerDisplayName, numShowsInHeader) {
+  let headerBarElm = Util.create('div', {class: 'header-bar'});
+  return headerBarElm;
+}
+
+function getShowBarElm(show) {
+  let showBarElm = Util.create('div', {class: 'show-bar'});
+  return showBarElm;
 }
