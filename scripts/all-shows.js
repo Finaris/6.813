e627c3b5c8f45798679930f1ab86b808d0ad3862
Michael Feffer <mfeffer@mit.edu>
2018-05-04 02:31:36 -0400

    Dropdowns added to all shows page, albeit sketch ones

--- scripts/all-shows.js
+++ scripts/all-shows.js
@@ -12,6 +12,39 @@ Util.events(document, {
   },
 });
 
+let currentDropDownMenuElm = null;
+
+let listNames = ['To Watch', 'Watching', 'Completed'];
+let listsToSections = {
+  'To Watch': ['Feels', 'Romance', 'Comedy', 'Action', 'Thriller', 'Psychological'],
+  'Watching': ['Completed Shows', 'Airing Shows'],
+  'Completed': ['Amazing', 'Good', 'Okay', 'Bad']
+};
+
+function addOptionToSelect(sel, name) {
+  option = Util.create('option', { value: name });
+  option.innerHTML = name;
+  sel.appendChild(option);
+}
+
+window.addEventListener('resize', function() {
+  // Moves the dropdown upon resize
+  if (currentDropDownMenuElm != null) {
+    currentDropDownMenuElm.style.setProperty('left', currentAddButtonElm.offsetLeft + 'px');
+    currentDropDownMenuElm.style.setProperty('top', (currentAddButtonElm.offsetTop + currentAddButtonElm.offsetHeight + 5) + 'px');
+  }
+});
+
+document.addEventListener('click', function(evt) {
+  // Makes dropdown disappear if clicked outside
+  if (currentDropDownMenuElm != null) {
+    if (!currentDropDownMenuElm.contains(evt.target)) {
+      removeAddShowDropdownMenu();
+    }
+  }
+});
+
+
 // initialize the page
 function initAllShowsPageDOM() {
   addShowsToDOM(ALL_SHOWS);
@@ -32,6 +65,7 @@ function addShowsToDOM(shows) {
     let imgElm = Util.create('img', { src: '../' + show.img, class: 'show-img' });
     let dataElm = getInfo(show);
     let buttonElm = Util.create('i', { class: 'all-show-add-btn fa fa-plus-circle' });
+    buttonElm.addEventListener('click', onAddButtonClick);
 
     showDisplayElm.appendChild(imgElm);
     showDisplayElm.appendChild(dataElm);
@@ -88,4 +122,68 @@ function onFilterButtonClick() {
 
   // resets screen view to the top
   Util.one('main').scrollTop = 0;
+}
+
+function onAddButtonClick(evt) {
+  evt.stopPropagation();
+  if (currentDropDownMenuElm == null) {
+    let dropdownMenuElm = Util.create('div', { class: 'dropdown-menu' });
+    let listSelectionElm = Util.create('div', { class: 'list-input-section' });
+    let listLabelElm = Util.create('div');
+    listLabelElm.innerHTML = "List: ";
+
+    let listSelect = Util.create('select', { id: 'list-select' });
+
+    listNames.forEach(function(elt) { addOptionToSelect(listSelect, elt) });
+
+    listSelect.onchange = function(e) {
+      let listSel = Util.one('#list-select');
+      let sectionSel = Util.one('#section-select');
+      while (sectionSel.firstChild) {
+        sectionSel.removeChild(sectionSel.firstChild);
+      }
+      listsToSections[listSel.options[listSel.selectedIndex].value].forEach(function(elt) { addOptionToSelect(sectionSel, elt) });
+    };
+
+    let sectionLabelElm = Util.create("div");
+    sectionLabelElm.innerHTML = "Section: ";
+
+    let sectionSelect = Util.create('select', { id: 'section-select' });
+    listsToSections["To Watch"].forEach(function(elt) { addOptionToSelect(sectionSelect, elt) });
+
+    listSelectionElm.appendChild(listLabelElm);
+    listSelectionElm.appendChild(listSelect);
+    listSelectionElm.appendChild(sectionLabelElm);
+    listSelectionElm.appendChild(sectionSelect);
+
+    //listSelectionElm.innerHTML = "List: ";
+    let submitButtonElm = Util.create('button', { class: 'btn btn-primary' });
+    submitButtonElm.innerHTML = 'Add';
+
+    dropdownMenuElm.style.setProperty('left', evt.target.offsetLeft + 'px');
+    dropdownMenuElm.style.setProperty('top', (evt.target.offsetTop + evt.target.offsetHeight + 5) + 'px');
+
+    dropdownMenuElm.appendChild(listSelectionElm);
+    dropdownMenuElm.appendChild(submitButtonElm);
+
+    Util.one('main').appendChild(dropdownMenuElm);
+    currentDropDownMenuElm = dropdownMenuElm;
+    currentAddButtonElm = evt.target;
+  } else {
+    let temp = currentAddButtonElm;
+    removeAddShowDropdownMenu();
+    if (temp != evt.target && evt.target.classList.contains('add-btn')) {
+      onAddButtonClick(evt);
+    }
+  }
+}
+
+//--------------------------------------- Helper Functions -----------------------------------------//
+
+function removeAddShowDropdownMenu() {
+  if (currentDropDownMenuElm != null) {
+    Util.one('main').removeChild(currentDropDownMenuElm);
+    currentDropDownMenuElm = null;
+    currentAddButtonElm = null;
+  }
 }
